0f840342354172d64efc3baeac9379892469710b
Andrea Sierra Rodríguez <4ndreasierra@gmail.com>
2023-03-25 12:09:35 +0100

    Nueva Modificacion (2)

--- Prueba_Ec/src/P1/Clase_Basica.java
+++ Prueba_Ec/src/P1/Clase_Basica.java
@@ -5,6 +5,7 @@ public class Clase_Basica {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Practicas repositorio local -> remoto en git hub");
+		System.out.println("Modificacion (2)");
 	}
 
 }
